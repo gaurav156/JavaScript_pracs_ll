72b1235f4d178b742b95115955aa9ae7566096ec
Gaurav Shukla <gaurav.shukla@global.internal>
2022-04-25 23:22:16 +0530

    03_09 - Object Methods

---
+++ 03/03_09_prac_ObjectMethod.js
@@ -0,0 +1,15 @@
+// const book = {
+//     name: "book 1",
+//     pageCount: 20,
+//     author: {
+//         firstName: "Gaurav",
+//         lastName: "Shukla",
+//     },
+//     pageColor: false,
+//     setPageColor: function (pageType){
+//         this.pageColor = pageType;
+//     }
+// };
+console.log("03_09-Object Method: ")
+book.setPageColor(true);
+console.log("page color is:",book.pageColor);
